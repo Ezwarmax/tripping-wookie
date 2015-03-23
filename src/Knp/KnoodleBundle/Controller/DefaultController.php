9e551b21eb8f844b3062e1b7b0b04b517dfcdafb
Ezwarmax <maximerouilly@MacBook-Pro-de-Maxime-5.local>
2015-03-23 12:02:53 +0100

    Bundle

---
+++ src/Knp/KnoodleBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Knp\KnoodleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('KnpKnoodleBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Knp/KnoodleBundle/KnpKnoodleBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Knp\KnoodleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KnpKnoodleBundle extends Bundle
+{
+}
